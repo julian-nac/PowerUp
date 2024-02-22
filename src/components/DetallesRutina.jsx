098f06c5a770f695d839dfaa38c445d7cb915e64
julian narvaez <julian.narvaezc@gmail.com>
2024-02-22 17:35:02 -0500

    Detalles de las rutinas terminadas

--- src/components/DetallesRutina.jsx
+++ src/components/DetallesRutina.jsx
@@ -1,79 +1,172 @@
+
 import React, { useState } from 'react';
-import { View, Text, Button, Image, StyleSheet, ScrollView } from 'react-native';
+
+import { View, Text, Button, TouchableOpacity, Image, StyleSheet, ScrollView, ImageBackground } from 'react-native';
+
 
 const DetallesRutina = ({ route, navigation }) => {
+
   const { rutina } = route.params;
+
   const [indiceEjercicio, setIndiceEjercicio] = useState(0);
 
+
   const handleSiguienteEjercicio = () => {
+
     if (indiceEjercicio < rutina.ejercicios.length - 1) {
+
       setIndiceEjercicio(indiceEjercicio + 1);
+
     } else {
-      // Redirigir a alguna pantalla cuando se completen todos los ejercicios
+
       navigation.navigate('Place');
+
     }
+
   };
 
+  const progreso = (indiceEjercicio + 1) / rutina.ejercicios.length;
+
+
   return (
-    <ScrollView style={styles.container}>
+
+<View style={styles.container}>
+
+    <ImageBackground
+        
+        source={require('../../assets/images/Fondo-pantalla-metal.jpg')}
+  
+        style={styles.backgroundImage}
+
+    >
+     
       <View style={styles.header}>
-        <Image source={{ uri: rutina.imagen }} style={styles.imagen} />
-        <Text style={styles.categoriaText}>{rutina.categoria}</Text>
+     
         <Text style={styles.zonaText}>{rutina.zona}</Text>
+     
       </View>
 
       <View style={styles.ejerciciosContainer}>
+     
         <View style={styles.ejercicioContainer}>
-          <Text style={styles.nombreText}>{rutina.ejercicios[indiceEjercicio].nombre}</Text>
+     
+          <Image
+     
+          source={{ uri: rutina.ejercicios[indiceEjercicio].gif }}
+          
+          style={styles.imagen}
+          
+          />
+          
+          <TouchableOpacity style={styles.circleButton} onPress={handleSiguienteEjercicio}>
+          
+            <Text style={styles.buttonText}>✓</Text>
+          
+          </TouchableOpacity>
+          
+          <Text style={styles.nombreText}>{rutina.ejercicios[indiceEjercicio].nombre}{` : ${rutina.ejercicios[indiceEjercicio].duracion}`}</Text>
+          
           <Text style={styles.descripcionText}>{rutina.ejercicios[indiceEjercicio].descripcion}</Text>
-          <Text style={styles.duracionText}>{`Duración: ${rutina.ejercicios[indiceEjercicio].duracion}`}</Text>
+        
         </View>
+      
       </View>
 
-      <Button title="Siguiente Ejercicio" onPress={handleSiguienteEjercicio} />
-    </ScrollView>
+      <View style={styles.progressBarContainer}>
+      
+        <View style={styles.progressBar}>
+      
+          <View style={{ width: `${progreso * 100}%`, height: '100%', backgroundColor: 'yellow' }} />
+      
+        </View>
+      
+      </View>
+     
+     </ImageBackground>
+    
+    </View>
+  
   );
+
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
   header: {
     alignItems: 'center',
-    marginTop: 20,
-  },
-  imagen: {
-    width: 100,
-    height: 100,
-    marginBottom: 10,
-  },
-  categoriaText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
+    marginTop: 0,
+    marginLeft: -10,
+    marginRight: -10,
+    backgroundColor: 'yellow',
+    borderColor: 'black',
+    borderWidth: 5,
+    padding: 20,
   },
   zonaText: {
-    fontSize: 16,
-    color: '#888',
+    fontSize: 22,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  imagen: {
+    width: 300,
+    height: 250,
+    marginBottom: 10,
+    alignSelf: 'center',
   },
   ejerciciosContainer: {
     padding: 20,
   },
   ejercicioContainer: {
     marginBottom: 20,
+    height: 550, 
   },
   nombreText: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
+    color: 'white',
+    marginTop: '5%',
+    textAlign: 'center'
   },
   descripcionText: {
-    fontSize: 14,
-    color: '#444',
+    fontSize: 15,
+    color: 'white',
+    marginTop: '5%',
+    textAlign: 'center'
   },
-  duracionText: {
-    fontSize: 12,
-    color: '#666',
+  circleButton: {
+    width: 80,
+    height: 80,
+    borderRadius: 125,
+    backgroundColor: 'yellow',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 5,
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  progressBarContainer: {
+    position: 'absolute',
+    top: 10,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 30,
+  },
+  progressBar: {
+    height: '100%',
   },
 });
 
