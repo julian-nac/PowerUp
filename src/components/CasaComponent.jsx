8b4a7a5be8192f6041385a42b03297519ea976cf
julian narvaez <julian.narvaezc@gmail.com>
2024-03-11 16:18:40 -0500

    Arreglo bugs

--- src/components/CasaComponent.jsx
+++ src/components/CasaComponent.jsx
@@ -1,70 +1,124 @@
+
 import React, { useState, useRef } from 'react';
+
 import { View, Text, Image, ImageBackground, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+
 import { useNavigation } from '@react-navigation/native';
+
 import { rutinas } from '../data/ejerciciosCasa.json';
 
+
 const CasaComponente = () => {
+
   const [zonaSeleccionada, setZonaSeleccionada] = useState(null);
-  const scrollViewRef = useRef(null); // Referencia al ScrollView
+
+  const scrollViewRef = useRef(null);
+
   const navigation = useNavigation();
 
   const zonasUnicas = [...new Set(rutinas.map((rutina) => rutina.zona))].filter(Boolean);
 
   const handleZonaPress = (zona) => {
+   
     setZonaSeleccionada(zona);
-    // Scroll automático hacia las rutinas
+   
     scrollViewRef.current?.scrollTo({ y: 1000, animated: true });
+  
   };
 
   const handleRutinaPress = (rutina) => {
+  
     navigation.navigate('Rutina', { rutina });
+  
   };
 
   return (
-    <View style={styles.container}>
+  
+  <View style={styles.container}>
+  
       <ImageBackground
-        source={require('../../assets/images/Fondo-pantalla-metal.jpg')}
-        style={styles.backgroundImage}
+  
+      source={require('../../assets/images/Fondo-pantalla-metal.jpg')}
+      
+      style={styles.backgroundImage}
+      
       >
+      
         <ScrollView ref={scrollViewRef}>
+      
           <View style={styles.filterContainer}>
+      
             <Text style={styles.filterText}>Seleccionar Zona:</Text>
+      
           </View>
 
+
           <View style={styles.zonasContainer}>
+
             {zonasUnicas.map((zona, index) => (
-              <TouchableOpacity
-                key={index}
-                style={styles.zonaContainer}
-                onPress={() => handleZonaPress(zona)}
-              >
-                <Image source={{ uri: rutinas.find((rutina) => rutina.zona === zona)?.banner }} style={styles.zonaImage} />
-              </TouchableOpacity>
-            ))}
+
+          <TouchableOpacity
+          
+          key={index}
+          
+          style={styles.zonaContainer}
+          
+          onPress={() => handleZonaPress(zona)}
+          
+          >
+          
+          <Image source={{ uri: rutinas.find((rutina) => rutina.zona === zona)?.banner }} style={styles.zonaImage} />
+    
+          </TouchableOpacity>
+
+          ))}
+          
           </View>
 
           {zonaSeleccionada && (
-            <View style={styles.rutinasContainer}>
+          
+          <View style={styles.rutinasContainer}>
+          
               <Text style={styles.rutinasText}>{`Rutinas para ${zonaSeleccionada}:`}</Text>
+          
               {rutinas
-                .filter((rutina) => rutina.zona === zonaSeleccionada)
-                .map((rutina, index) => (
-                  <TouchableOpacity
-                    key={index}
-                    style={styles.rutinaContainer}
-                    onPress={() => handleRutinaPress(rutina)}
-                  >
-                    <Image source={{ uri: rutina.imagen }} style={styles.rutinaImage} />
-                    <Text style={styles.rutinaText}>{`Ejercicios: ${rutina.ejercicios.length}`}</Text>
-                    <Text style={styles.rutinaText}>{`Categoría: ${rutina.categoria}`}</Text>
-                  </TouchableOpacity>
-                ))}
+          
+          .filter((rutina) => rutina.zona === zonaSeleccionada)
+          
+          .map((rutina, index) => (
+          
+            <TouchableOpacity
+          
+            key={index}
+          
+            style={styles.rutinaContainer}
+          
+            onPress={() => handleRutinaPress(rutina)}
+          
+          >
+          
+          <Image source={{ uri: rutina.imagen }} style={styles.rutinaImage} />
+          
+          <Text style={styles.rutinaText}>{`Ejercicios: ${rutina.ejercicios.length}`}</Text>
+          
+          <Text style={styles.rutinaText}>{`Categoría: ${rutina.categoria}`}</Text>
+          
+         </TouchableOpacity>
+  
+          ))}
+          
             </View>
+          
           )}
+        
         </ScrollView>
+      
       </ImageBackground>
+    
     </View>
+  
   );
+
 };
 
 
@@ -96,7 +150,7 @@ const styles = StyleSheet.create({
   zonaContainer: {
     alignItems: 'center',
     margin: 2,
-    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    backgroundColor: 'rgba(0, 0, 0, 0.93)',
     borderRadius: 8,
   },
   zonaImage: {
@@ -108,27 +162,34 @@ const styles = StyleSheet.create({
   },
   rutinasContainer: {
     marginTop: 16,
+    padding: 16,
   },
   rutinasText: {
-    fontSize: 18,
+    fontSize: 20,
+    padding: 20,
     fontWeight: 'bold',
+    backgroundColor: 'black',
+    color: 'white',
+    borderBottomColor: 'yellow',
+    borderWidth: 2,
     marginBottom: 8,
   },
   rutinaContainer: {
     alignItems: 'center',
     marginVertical: 8,
     padding: 16,
-    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    backgroundColor: 'rgba(0, 0, 0, 0.41)',
     borderRadius: 8,
   },
   rutinaImage: {
-    width: 100, // Ajusta el ancho según tus necesidades
-    height: 100, // Ajusta la altura según tus necesidades
-    borderRadius: 50,
+    width: 90, // Ajusta el ancho según tus necesidades
+    height: 90, // Ajusta la altura según tus necesidades
     marginBottom: 8,
   },
   rutinaText: {
-    fontSize: 16,
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold'
   },
 });
 
